aff22d1cfb0eade1fc2b82b73cb802c2a71c8a1e
pascal-lc <pascal.lc@outlook.com>
2023-10-06 18:04:22 +0800

    HJ56 完全数计算  优化

--- solutions/HJ56_1.java
+++ solutions/HJ56_1.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class HJ56 {
+public class HJ56_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextInt()) {
@@ -17,33 +16,17 @@ public class HJ56 {
     private static int perfectNumber(int n) {
         int count = 0;
         for (int i = 1; i <= n; i++) {
-            List<Integer> list = new ArrayList<>();
+            int sum = 0;
             for (int j = 1; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
-                    list.add(j);
-                    list.add(i / j);
+                    sum += j + i / j;
                 }
             }
-            if (listSum(list) == 2 * i) {
+            if (sum == 2 * i) {
                 count++;
-                // for (int k : list) {
-                //     System.out.print(k + " ");
-                // }
-                // System.out.println();
-                // System.out.println(list);
             }
         }
 
         return count - 1;
-    }
-
-    private static int listSum(List<Integer> list) {
-        int sum = 0;
-        sum = list.stream().reduce(0, Integer::sum);
-        // for (int i : list) {
-        //     sum += i;
-        // }
-
-        return sum;
     }
 }
